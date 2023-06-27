d9e228074bd1f3bf4b087e189011135954798e1f
SmileJun <godhkekf24@inha.edu>
2023-06-27 15:59:39 +0900

    [DAD-294] chore: 프로젝트 기본 설정 파일 생성

---
+++ src/main/java/com/forever/dadamdatest/DadamdaTestApplication.java
@@ -0,0 +1,13 @@
+package com.forever.dadamdatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadamdaTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadamdaTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forever/dadamdatest/DadamdaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forever.dadamdatest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadamdaTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
